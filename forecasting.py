868ca36f2d85b42f66098973b4994cc03cd3e2f8
boyuan <fengboyuannj@gmail.com>
2018-07-26 15:56:01 -0400

    Create project, experiment on predict multiple temp at the same time

---
+++ forecasting.py
@@ -0,0 +1,100 @@
+import os
+data_dir = 'C:\Boyuan\Machine Learning\Datasets\jena_climate_2009_2016'
+fname = os.path.join(data_dir, 'jena_climate_2009_2016.csv')
+
+f = open(fname)
+data = f.read()
+f.close()
+
+lines = data.split('\n')
+header = lines[0].split(',')
+lines = lines[1:]
+
+print(header)
+print(len(lines))
+
+
+
+import numpy as np
+
+float_data = np.zeros((len(lines), len(header) - 1))
+for i, line in enumerate(lines):
+    values = [float(x) for x in line.split(',')[1:]]
+    float_data[i, :] = values
+
+
+
+from matplotlib import pyplot as plt
+
+temp = float_data[:, 1]
+print(len(temp))
+plt.plot(range(len(temp)), temp)
+
+
+def generator(data, lookback, delay, min_index, max_index,
+              shuffle=False, batch_size=128, step=6):
+    if max_index is None:
+        max_index = len(data) - delay - 1
+    i = min_index + lookback
+    while 1:
+        if shuffle:
+            rows = np.random.randint(
+                min_index + lookback, max_index, size=batch_size)
+        else:
+            if i + batch_size >= max_index:
+                i = min_index + lookback
+            rows = np.arange(i, min(i + batch_size, max_index))
+            i += len(rows)
+
+        samples = np.zeros((len(rows), lookback // step, data.shape[-1]))
+        targets = np.zeros((len(rows),))
+        for j, row in enumerate(rows):
+            indices = range(rows[j] - lookback, rows[j], step)
+            samples[j] = data[indices]
+            targets[j] = data[rows[j] + delay][1]
+        yield samples, targets
+
+lookback = 1440
+step = 6
+delay = 144
+batch_size = 128
+
+train_gen = generator(float_data,
+                      lookback=lookback,
+                      delay=delay,
+                      min_index=0,
+                      max_index=200000,
+                      shuffle=True,
+                      step=step,
+                      batch_size=batch_size)
+val_gen = generator(float_data,
+                    lookback=lookback,
+                    delay=delay,
+                    min_index=200001,
+                    max_index=300000,
+                    shuffle=True,
+                    step=step,
+                    batch_size=batch_size)
+test_gen = generator(float_data,
+                    lookback=lookback,
+                    delay=delay,
+                    min_index=300001,
+                    max_index=None,
+                    shuffle=True,
+                    step=step,
+                    batch_size=batch_size)
+
+val_steps = (300000 - 200001 - lookback)
+
+test_steps = (len(float_data) - 300001 - lookback)
+
+def evaluate_naive_method():
+    batch_maes = []
+    for step in range(val_steps):
+        samples, targets = next(val_gen)
+        preds = samples[:, -1, 1]
+        mae = np.mean(np.abs(preds - targets))
+        batch_maes.append(mae)
+    print(np.mean(batch_maes))
+
+evaluate_naive_method()
